eb8a2eaaabc4662bdd4e6c26b3f9563edd55bf73
tarifalhasan <tarifalhasan999@gmail.com>
2024-03-15 10:27:23 +0600

    services page hero page fixture

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -1,6 +1,7 @@
 import Services from "@/components/pages/home/services";
 import Location from "@/components/shared/Location";
 import { Metadata } from "next";
+import Image from "next/image";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://stoma-elite.vercel.app/"),
@@ -21,6 +22,26 @@ export const metadata: Metadata = {
 const ServicesPage = () => {
   return (
     <>
+      <section className="  py-10 lg:py-14  container flex flex-col lg:flex-row gap-10 lg:items-center">
+        <div className=" basis-full lg:basis-1/2 space-y-5 lg:space-y-8">
+          <h2 className=" max-w-[565px] text-3xl xl:text-5xl font-bold">
+            Servicii
+          </h2>
+          <p className=" max-w-[498px] text-sm font-medium text-skin-gray lg:text-base">
+            Oferim o gama larga de servicii stomatologice. Suntem specializati
+            in chirurgie si implantologie.
+          </p>
+        </div>
+        <div className=" basis-full lg:basis-1/2">
+          <Image
+            className=" max-w-[600px] w-full h-full object-cover rounded-full"
+            width={903}
+            height={602}
+            src={"/images/services.webp"}
+            alt="hero"
+          />
+        </div>
+      </section>
       <Services className=" !bg-none" />
       <Location />
     </>
